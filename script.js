f4377f53576afd3fd3e3a19c91634ae3586c1398
RohitPandey1729 <168243805+RohitPandey1729@users.noreply.github.com>
2024-07-18 22:40:14 +0000

    I finished my script.js file and am working out some last bugs

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-  function getItinerary(e) {
+function getItinerary(e) {
 
-  e.preventDefault()
+    e.preventDefault()
 
-  console.log(e.target.location.value)
+    console.log(e.target.location.value)
 
-  fetch('https://api.openai.com/v1/chat/completions', {
-    
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer OPEN_AI_API_KEY'
-      },
+    fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer OPEN_AI_API_KEY'
+        },
 
-      body: JSON.stringify({
-          'model': 'gpt-3.5-turbo',
-          'messages': [{
-              'role': 'user',
-              'content': `plan a trip itinerary for someone going to ${e.target.location.value} from ${e.target.startdate.value} to ${e.target.enddate.value}. have about 3 or 4 things to do per day. respond ONLY with an array that has JSON objects with the parameters \`date\` \`eventTitle\` \`startTime\` \`endTime\`
-        \`\`\`
-        `
-          }],
-      })
-  }).then(result => result.json()).then(eventsResponse => {
+        body: JSON.stringify({
+            'model': 'gpt-3.5-turbo',
+            'messages': [{
+                'role': 'user',
+                'content': `plan a trip itinerary for someone going to ${e.target.location.value} from ${e.target.startdate.value} to ${e.target.enddate.value}. have about 3 or 4 things to do per day. respond ONLY with an array that has JSON objects with the parameters \`date\` \`eventTitle\` \`startTime\` \`endTime\`
+          \`\`\`
+          `
+            }],
+        })
+    }).then(result => result.json()).then(eventsResponse => {
 
-      const events = JSON.parse(eventsResponse.[0].message.content)
-      let htmlGenerated = ""
-          for (const event of events) {
-              //add it on
-              htmlGenerated += `<div>
-        <h3>${event.eventTitle}</h3>
-        <p>${new Date(event.date).toLocaleDateString(undefined, {dateStyle: "medium" })}</p>
+        const events = JSON.parse(eventsResponse.choices[0].message.content)
+        let htmlGenerated = ""
+        for (const event of events) {
+            //add it on
+            htmlGenerated += `<div>
+      <h3>${event.eventTitle}</h3>
+      <p>${new Date(event.date).toLocaleDateString(undefined, { dateStyle: "medium" })}</p>
 
-        <p>${event.startTime} - ${event.endTime}</p>
+      <p>${event.startTime} - ${event.endTime}</p>
 
-        </div>`
+      </div>`
+        }
+
+        document.getElementById("eventlist").innerHTML = htmlGenerated
+    });
+}
